9563be03dde2acae8c7be7142ec2feda775a750a
Jesse Stricker <git@jessestricker.de>
2026-06-06 00:11:48 +0200

    feat: add cli parsing, http server, state crypto

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "log"
+import (
+	"log/slog"
+	"os"
+	"strconv"
+
+	"github.com/jessestricker/terraform-backend-age/internal"
+)
 
 func main() {
-	log.Println("Hello, world!")
+	if debug, err := strconv.ParseBool(os.Getenv("TF_BACKEND_AGE_DEBUG")); err == nil && debug {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+	}
+
+	os.Exit(internal.Main())
 }
